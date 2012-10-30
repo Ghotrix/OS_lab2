e154f91251e3550393adb460eb78dfb08456c688
Valeriy Huz <ghotrix@gmail.com>
2012-10-30 22:51:55 +0200

    some tryings

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#define NUM_PIPES 4
-#define NUM_APPS 5//NUM_PIPES + 1
+#define NUM_PIPES 4
+#define NUM_APPS NUM_PIPES + 1
 
 int main(int argc, char * argv[])
 {
-	int fd[NUM_PIPES][2], status;
+	int fd[NUM_APPS][2], status;
 	char *app_names[] = {"cat", "grep", "awk", "sed", "sed", "grep"};
 	char *arg_names[] = {"log.txt", "10/Oct/2006", "{print $12}", "s/\"//g", "s/\\/.*//g", "1"};
 	pid_t child_pids[NUM_APPS];
 
-	for (int i = 0; i < NUM_PIPES; i++) {
+	for (int i = 0; i < NUM_APPS; i++) {
 		if(pipe(fd[i]) == -1) {
 			fprintf(stderr, "Cannot pipe...\n");
 			exit(EXIT_FAILURE);
@@ -31,36 +31,36 @@ int main(int argc, char * argv[])
 					fprintf(stderr, "Cannot dup2 stdin...\n");
 				close(fd[i-1][0]);
 			}
-			if (i != NUM_APPS - 1) {
+			//if (i != NUM_APPS - 1) {
 				close(fd[i][0]);
 				if(dup2(fd[i][1], 1) < 0)
 					fprintf(stderr, "Cannot dup2 stdout...\n");
-				if (i != NUM_PIPES - 1)
+				if (i != NUM_APPS - 1)
 					close(fd[i][1]);
-			}
+			//}
 			
-		for (int j = 0; j < NUM_PIPES; j++) {
+		for (int j = 0; j < NUM_APPS; j++) {
 			close(fd[j][0]);
-			if (i != NUM_APPS - 1 && j != NUM_PIPES - 1)
+			if (i != NUM_APPS - 1 || j != NUM_APPS - 1)
 				close(fd[j][1]);
 		}
 
 			char* command[3] = {app_names[i], arg_names[i], NULL};
 			execvp(command[0], command);
 			_exit(EXIT_FAILURE);
-		} 
+		}
 		else if (child_pids[i] == -1) { /* Cannot fork */
 			fprintf(stderr, "Can't fork, exiting...\n");
 			_exit(EXIT_FAILURE);
 		}
 	}
 
-	/*
+//	/*
 
-	close(fd[3][1]);
-	if (dup2(fd[3][0], 0) < 0)
+	close(fd[NUM_APPS - 1][1]);
+	if (dup2(fd[NUM_APPS - 1][0], 0) < 0)
 		fprintf(stderr, "Cannot dup2 stdin...\n");
-	close(fd[3][0]);
+	//close(fd[4][0]);
 
 	char buf[1024];
 
@@ -68,9 +68,9 @@ int main(int argc, char * argv[])
 		printf("%s\n", buf);
 	}
 
-	*/
+//	*/
 
-	for (int i = 0; i < NUM_PIPES; i++) {
+	for (int i = 0; i < NUM_APPS; i++) {
 		close(fd[i][0]);
 		close(fd[i][1]);
 	}
